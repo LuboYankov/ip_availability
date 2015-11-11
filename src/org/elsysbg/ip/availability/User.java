b81174e7f9228124e6acbd72f7e7f418afe8e203
LuboYankov <ahhak123@gmail.com>
2015-11-11 21:52:41 +0200

    Adding User class variables and constructor

--- src/org/elsysbg/ip/availability/User.java
+++ src/org/elsysbg/ip/availability/User.java
@@ -1,5 +1,15 @@
 package org.elsysbg.ip.availability;
 
 public class User {
-
+	
+	private String name;
+	private int loginCount;
+	private boolean in;
+	
+	public User(String username) {
+		this.name = username;
+		this.loginCount = 0;
+		this.in = false;
+	}
+	
 }
